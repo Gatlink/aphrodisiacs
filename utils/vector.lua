523efc39c592aff504d1b18f32787c877626d579
Sébastien Gatty <sebastien.gatty@naturalpad.com>
2015-03-02 20:47:16 +0100

    New vector functions, optimised lerp

--- utils/vector.lua
+++ utils/vector.lua
@@ -4,13 +4,9 @@ Vector.__index = Vector
 function Vector.new(x, y)
 	local new
 
-	if type(x) == "table" and x.__class == "Vector" then
-		new = {x = x.x, y = x.y}
-	else
-		x = x or 0
-		y = y or 0
-		new = {x = x, y = y}
-	end
+  x = x or 0
+  y = y or 0
+  new = {x = x, y = y}
 
 	setmetatable(new, Vector)
 	return new
@@ -21,25 +17,23 @@ Vector.__tostring = function (self)
 end
 
 Vector.__add = function (lhs, rhs)
-	if lhs.__class == "Vector" and type(rhs) == "number" then
+	if type(rhs) == "number" then
 		return Vector.new(lhs.x + rhs, lhs.y + rhs)
-	elseif lhs.__class == "Vector" and rhs.__class == "Vector" then
+	else
 		return Vector.new(lhs.x + rhs.x, lhs.y + rhs.y)
 	end
 end
 
 Vector.__sub = function (lhs, rhs)
-	if lhs.__class == "Vector" and type(rhs) == "number" then
+	if type(rhs) == "number" then
 		return Vector.new(lhs.x - rhs, lhs.y - rhs)
-	elseif lhs.__class == "Vector" and rhs.__class == "Vector" then
+	else
 		return Vector.new(lhs.x - rhs.x, lhs.y - rhs.y)
 	end
 end
 
 Vector.__unm = function (rhs)
-	if (rhs.__class == "Vector") then
-		return Vector.new(-rhs.x, -rhs.y)
-	end
+  return Vector.new(-rhs.x, -rhs.y)
 end
 
 Vector.__mul = function (lhs, rhs)
@@ -49,10 +43,8 @@ Vector.__mul = function (lhs, rhs)
 end
 
 function Vector:copy(oth)
-	if type(oth) == "table" and oth.__class == "Vector" then
-		self.x = oth.x
-		self.y = oth.y
-	end
+  self.x = oth.x
+  self.y = oth.y
 end
 
 function Vector:clone()
@@ -66,13 +58,13 @@ function Vector:norm()
 	return math.sqrt(self.x^2 + self.y^2)
 end
 
-function Vector:normalize()
+function Vector:normalized()
 	local norm = self:norm()
 	return Vector.new(self.x / norm, self.y / norm)
 end
 
 function Vector:dot(oth)
-	return self:norm() * oth:norm() * math.cos(math.abs(self:angle(oth)))
+	return self.x * oth.x + self.y * oth.y
 end
 
 function Vector:rotate(angle)
@@ -90,7 +82,20 @@ function Vector:set(x, y)
   self.y = y
 end
 
-Vector.__class = "Vector"
+function Vector:normal()
+  return Vector.new(-self.y, self.x)
+end
+
+function Vector:det(oth)
+  return self.x * oth.y - self.y * oth.x
+end
+
+function Vector:projectOn(oth)
+  local normalizedOth = oth:normalized()
+  return normalizedOth * self:dot(normalizedOth)
+end
+
+Vector.zero = Vector.new()
 Vector.up = Vector.new(0, -1)
 Vector.down = Vector.new(0, 1)
 Vector.left = Vector.new(-1, 0)
